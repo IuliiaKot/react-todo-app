0e5acc3a66245ba5143f37bd7975b792853ea511
IuliiaKot <kotlenko.julia@gmail.com>
2017-04-28 16:22:19 -0400

    add state with hardcode data

--- src/App.js
+++ src/App.js
@@ -3,6 +3,18 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      todos: [
+        {id: 1, name: 'Learn JSX', isComplete: true},
+        {id: 2, name: 'Build an Awesome App', isComplete: false},
+        {id: 3, name: 'Ship It!', isComplete: false},
+
+      ]
+    }
+  }
+
   render() {
     return (
       <div className="App">
@@ -16,9 +28,13 @@ class App extends Component {
           </form>
           <div className="Todo-List">
             <ul>
-              <li><input type="checkbox"/> Learn JSX</li>
-              <li><input type="checkbox"/> Build an Awesome App</li>
-              <li><input type="checkbox"/> Ship It!</li>
+              {
+                this.state.todos.map(item => {
+                  return <li key={item.id}><input
+                    type="checkbox"
+                    defaultChecked={item.isComplete}/>{item.name}</li>
+                })
+              }
             </ul>
           </div>
         </div>
